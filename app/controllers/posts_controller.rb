aff6b2e5c923adcbdf18e7e8fad0e9c95b0c6821
Kanayama-Risa <cosmos.24kix3@gmail.com>
2021-09-08 13:58:09 +0000

    make controller&view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,24 @@
 class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
 
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    @post.save
+    redirect_to posts_path
+  end
+
+  def index
+    @post = Post.all
+  end
+
+  def show
+  end
+
+  def destroy
+  end
 
 
   def post_params
