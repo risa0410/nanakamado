b9f77bab85cfbf484036fbc9d7e560a6f5e48358
Kanayama-Risa <cosmos.24kix3@gmail.com>
2021-09-13 11:37:03 +0000

    update user show&edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,23 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to user_path(current_user)
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    byebug
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+      # flash[:notice] = "You have updated user successfully."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -26,13 +26,13 @@ class User < ApplicationRecord
   end
 
   has_one_attached :profile_image
-  validate :profile_image_type
+  # validate :profile_image_type
 
-  private
-  def profile_image_type
-    if !profile_image.blob.content_type.in?(%('profile_image/jpeg profile_image/png'))
-      profile_image.purge # Rails6では、この1行は必要ない
-      errors.add(:profile_image, 'はJPEGまたはPNG形式を選択してアップロードしてください')
-    end
-  end
+  # private
+  # def profile_image_type
+  #   if !profile_image.blob.content_type.in?(%('profile_image/jpeg profile_image/png'))
+  #     profile_image.purge # Rails6では、この1行は必要ない
+  #     errors.add(:profile_image, 'はJPEGまたはPNG形式を選択してアップロードしてください')
+  #   end
+  # end
 end
