5f373acd7ec81fa416253b2ced8e100d63b476e7
Kanayama-Risa <cosmos.24kix3@gmail.com>
2021-09-20 08:02:50 +0000

    update view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,7 +18,7 @@ class PostsController < ApplicationController
 
   def index
     @user = current_user
-    @post = Post.all.order(created_at: :desc)
+    @post = Post.all.page(params[:page]).reverse_order
   end
 
   def show
