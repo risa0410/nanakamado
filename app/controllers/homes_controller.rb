dbd2964f272f46eab0772b73f042e4821b3c92ab
Kanayama-Risa <cosmos.24kix3@gmail.com>
2021-10-03 09:39:29 +0000

    update test

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,7 @@
 class HomesController < ApplicationController
 
   def top
-    @posts = Post.limit(4).order(" created_at DESC ")
+    @posts = Post.limit(4).order(created_at: :desc)
   end
 
   def about
--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,6 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe 'Relationshipモデルのテスト', type: :model do
+  let(:relationship) { FactoryBot.create(:relationship) }
+  describe '#create' do
+    context "保存できる場合" do
+      it "全てのパラメーターが揃っていれば保存できる" do
+        expect(relationship).to be_valid
+      end
+    end
+
+    context "保存できない場合" do
+      it "follower_idがnilの場合は保存できない" do
+        relationship.follower_id = nil
+        relationship.valid?
+        expect(relationship.errors[:follower]).to include("を入力してください")
+      end
+
+      it "followed_idがnilの場合は保存できない" do
+        relationship.followed_id = nil
+        relationship.valid?
+        expect(relationship.errors[:followed]).to include("を入力してください")
+      end
+    end
+  end
 
   describe 'アソシエーションのテスト' do
     context 'Followerモデルとの関係' do
